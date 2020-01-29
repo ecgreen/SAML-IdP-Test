d63e5084a7d2f5f1c484e8d72e4bddacbec66039
Ethan Green <ethan.c.green@gmail.com>
2020-01-29 15:28:47 -0800

    Not perfect, but this does seem to be functioning now

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -1,9 +1,39 @@
+require 'rest-client'
+require 'repost'
+include SamlIdp
+
 class SamlIdpController < SamlIdp::IdpController
+
+	def new
+		puts "in new"
+		render :template => "saml_idp/idp/new"
+	end
+
+	def create
+		puts "in create"
+		unless params[:email].blank? && params[:password].blank?
+        	person = idp_authenticate(params[:email], params[:password])
+			if person.nil?
+				@saml_idp_fail_msg = "Incorrect email or password."
+			else
+				puts "in create else"
+				@saml_response = idp_make_saml_response(person)
+
+				puts saml_acs_url
+				return redirect_post(saml_acs_url, params: { SAMLResponse: @saml_response })
+				# puts response_url
+				# return redirect_to response_url
+
+        	end
+      	end
+    	render :template => "saml_idp/idp/new"
+	end
+
 	def idp_authenticate(email, password)
-		true
+		{ email: email }
 	end
 
 	def idp_make_saml_response(user)
-		encode_SAMLResponse("you@example.com")
+		encode_SAMLResponse(user[:email])
 	end
 end
